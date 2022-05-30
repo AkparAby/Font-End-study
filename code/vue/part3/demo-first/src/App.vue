0ae9b42e6027f11aced7738a4a27bd8636c24ed5
Akpar <AkparAby@outlook.com>
2022-05-30 18:04:18 +0800

    newfile

---
+++ code/vue/part3/demo-first/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <p>11</p>
+    <p>22</p>
+    <p>333</p>
+  </div>
+</template>
---
+++ code/vue/part3/demo-first/src/test.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="box">
+    <h3>这是用户自定义的test页面----{{ username }}</h3>
+  </div>
+</template>
+
+<script>
+// 默认导出 这是固定写法!
+export default {
+  //data 数据源
+  //注意: .vue 组件中的 data 不能像之前一样 不能指向对象
+  //注意: .vue 组件中的 data 必须是一个函数
+  data() {
+    //这个 return 出去的 {} 中可以定义数据
+    return {
+      username: "admin",
+    };
+  },
+};
+</script>
+
+<style>
+#box {
+  background-color: aquamarine;
+}
+</style>
